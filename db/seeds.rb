a4d872b559bd9e7992279992b2f60b43ef08930e
Luiz Bernardes <soumaisluiz@hotmail.com>
2019-03-30 21:13:32 -0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Proposal.destroy_all
+
+Proposal.create(name: 'proposta 1',description: 'descrição da proposta 1', \
+	value: 2.25,date_begin: '01-01-2019',date_end: '01-08-2019',city: 'Rio de Janeiro')
+
+Proposal.create(name: 'proposta 2',description: 'descrição da proposta 2', \
+	value: 2.25,date_begin: '01-01-2019',date_end: '01-08-2019',city: 'São Paulo')
+
+Proposal.create(name: 'proposta 3',description: 'descrição da proposta 3', \
+	value: 2.25,date_begin: '01-01-2019',date_end: '01-08-2019',city: 'Belo Horizonte')
+
+Proposal.create(name: 'proposta 4',description: 'descrição da proposta 4', \
+	value: 2.25,date_begin: '01-01-2019',date_end: '01-08-2019',city: 'Vitória')
+
+Proposal.create(name: 'proposta 5',description: 'descrição da proposta 5', \
+	value: 2.25,date_begin: '01-01-2019',date_end: '01-08-2019',city: 'Curitiba')
